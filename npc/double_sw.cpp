0085807f529c1b3797a607cb529b063ca51dab01
tracer-ysyx <tracer@ysyx.org>
2025-12-22 22:58:10 +0800

    > sim RTL ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:58:10 up 1 day,  6:01,  1 user,  load average: 0.34, 0.33, 0.39

---
+++ npc/double_sw.cpp
@@ -0,0 +1,28 @@
+#include "Vdouble_sw.h"
+#include "verilated.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
+
+int main(int argc, char **argv) {
+
+    VerilatedContext *contextp = new VerilatedContext;
+    contextp->commandArgs(argc, argv);
+    Vdouble_sw *top = new Vdouble_sw{contextp};
+
+    while (!contextp->gotFinish()) {
+	int a = rand() & 1;
+	int b = rand() & 1;
+	top->a = a;
+	top->b = b;
+	top->eval();
+	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
+	assert(top->f == (a ^ b));
+    }
+
+    delete top;
+    delete contextp;
+
+    return 0;
+
+}
