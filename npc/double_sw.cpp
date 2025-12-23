02b3102b5503a49a1df68d63e09885c2c5b8f1ae
tracer-ysyx <tracer@ysyx.org>
2025-12-23 11:03:43 +0800

    > sim RTL ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:03:43 up 1 day, 18:06,  1 user,  load average: 0.32, 0.26, 0.32

--- npc/double_sw.cpp
+++ npc/double_sw.cpp
@@ -1,16 +1,27 @@
 #include "Vdouble_sw.h"
 #include "verilated.h"
+#include "verilated_vcd_c.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <assert.h>
 
 int main(int argc, char **argv) {
+    const std::unique_ptr<VerilatedContext> contextp{new VerilatedContext};
 
     VerilatedContext *contextp = new VerilatedContext;
     contextp->commandArgs(argc, argv);
     Vdouble_sw *top = new Vdouble_sw{contextp};
 
-    while (!contextp->gotFinish()) {
+    Verilated::traceEverOn(true);
+    VerilatedVcdC* tfp = new VerilatedVcdC;
+    topp->trace(tfp,99);
+    tfp->open("obj_dir/t_trace_ena_cc/simx.vcd");
+
+    while (contextp->time() < sim_time && !contextp->gotFinish()) {
+	contextp->timeInc(1);
+	topp->eval();
+	tfp->dump(contextp->time());
+
 	int a = rand() & 1;
 	int b = rand() & 1;
 	top->a = a;
@@ -19,7 +30,7 @@ int main(int argc, char **argv) {
 	printf("a = %d, b = %d, f = %d\n", a, b, top->f);
 	assert(top->f == (a ^ b));
     }
-
+    tfp->close();
     delete top;
     delete contextp;
 
