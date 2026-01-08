6592cc18468b833dee416cb58aaefc45517d0b0c
tracer-ysyx <tracer@ysyx.org>
2026-01-08 23:50:00 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:50:00 up 8 days,  7:46,  1 user,  load average: 0.23, 0.27, 0.60

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -219,6 +219,7 @@ word_t expr(char *e, bool *success) {
 
   *success = true;
   uint32_t str_len = strlen(e);
+  for (int k = 0; k < str_len; k++) printf("the tokens are %s", tokens[k].str);
   return (eval(0, str_len));
   /* TODO: Insert codes to evaluate the expression. */
 }
