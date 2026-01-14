facbf9630fe1f56230f5c61374d71ec3b1816df0
tracer-ysyx <tracer@ysyx.org>
2026-01-14 14:40:08 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:40:08 up 14 days,  6:36,  1 user,  load average: 0.48, 0.54, 0.40

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -31,12 +31,9 @@ void isa_reg_display() {
 
 word_t isa_reg_str2val(const char *s, bool *success) {
   *success = false;
-  printf("The s are: %s\n", s);
   for (int i = 0; i < 32; i ++) {
-    printf("reg[%d]: %s\n", i, regs[i]);
     if (strcmp(regs[i], s) == 0) {
       *success = true;
-      printf("----good\n");
       return *(regs[i]);
     }
   }
