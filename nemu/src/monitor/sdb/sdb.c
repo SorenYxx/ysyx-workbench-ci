aa2edc1fe7d80108976a08f157b194676e76c2cd
tracer-ysyx <tracer@ysyx.org>
2026-01-17 10:03:54 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-36-generic #36~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Oct 15 15:45:17 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:03:54 up 17 days,  2:00,  1 user,  load average: 0.18, 0.31, 0.33

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -73,8 +73,8 @@ static int cmd_info(char *args) {
   char b[] = {"rw"};
   for (int i = 0; i < 1; i ++) {
     switch (b[i]) {
-    case 'r': isa_reg_display(); return 0;
-    case 'w': return c_print();
+      case 'r': printf("args is : %c\n", b[i]); isa_reg_display(); return 0;
+      case 'w': return c_print();
     }
   }
   printf("Unknown usage: '%s'\n", args);
