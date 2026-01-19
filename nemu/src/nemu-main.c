5898fa5819665fe21dad5710bdd2bc7966f0828e
tracer-ysyx <tracer@ysyx.org>
2026-01-19 09:55:02 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  09:55:02 up 23:47,  1 user,  load average: 0.82, 0.67, 0.42

--- nemu/src/nemu-main.c
+++ nemu/src/nemu-main.c
@@ -29,7 +29,7 @@ int main(int argc, char *argv[]) {
   init_monitor(argc, argv);
 #endif
 
-  FILE *fp = fopen("input.txt", "r");
+  FILE *fp = fopen("input", "r");
   printf("1\n");
 
   if (!fp) {
