2d9e5bc773af98df4f0cb7c1e7d26267de020cd0
tracer-ysyx <tracer@ysyx.org>
2026-01-22 18:13:39 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  18:13:39 up 4 days, 16:06,  1 user,  load average: 0.81, 1.30, 1.07

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -33,10 +33,10 @@ enum {
 #define immU() do { *imm = SEXT(BITS(i, 31, 12), 20) << 12; } while(0)
 #define immS() do { *imm = (SEXT(BITS(i, 31, 25), 7) << 5) | BITS(i, 11, 7); } while(0)
 #define immJ() do { \
-  *imm = (SEXT((BITS(i, 31, 31) << 11) | \
-               (BITS(i, 19, 12)) | \
-               (BITS(i, 20, 20) << 10) | \
-               (BITS(i, 30, 21)), 20) << 1); \
+  *imm = (SEXT((BITS(i, 31, 31) << 20) |  \
+               (BITS(i, 30, 21) << 1) |   \
+               (BITS(i, 20, 20) << 11) |  \
+               (BITS(i, 19, 12) << 12), 21) << 1); \
 } while(0)
 
 static void decode_operand(Decode *s, int *rd, word_t *src1, word_t *src2, word_t *imm, int type) {
