ff6ce7776d68e781709d69bc42f682a348edad77
tracer-ysyx <tracer@ysyx.org>
2026-01-23 16:59:36 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:59:36 up 5 days, 22:53,  1 user,  load average: 0.44, 0.43, 0.60

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -94,13 +94,13 @@ static int cmd_x(char *args) {
   char *arg0 = strtok(NULL, " ");//arg0 is the frist argv
   char *arg1 = strtok(NULL, " ");//arg1 is the second argv
 
+  printf("1\n");
   if (arg0 != NULL) {
     n = strtol(arg0, &a0, 10);
-    if (arg1 != NULL) s = strtol(arg1, &a1, 0);
-    else s = 0;
+    s = (arg1 == NULL) ? 0: strtol(arg1, &a1, 0);
 
     if (a0 == arg0 || *a0 != '\0' || n <= 0 || a1 == arg1) printf("Error: Unknown usage: '%s'\n", arg);
-
+    printf("2\n");
     vaddr_t start = cpu.pc;
     for (vaddr_t i = start; i < 0X88000000 ; i += 4) {
       if (!(s != i)) {
