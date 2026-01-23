e3d73cda6b115035f703d2e8b15293e2b346705a
tracer-ysyx <tracer@ysyx.org>
2026-01-23 17:03:54 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:03:54 up 5 days, 22:57,  1 user,  load average: 0.50, 0.42, 0.55

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -96,16 +96,13 @@ static int cmd_x(char *args) {
 
   if (arg0 != NULL) {
     n = strtol(arg0, &a0, 10);
-    s = (arg1 == NULL) ? 0: strtol(arg1, &a1, 0);
+    s = (arg1 == NULL) ? cpu.pc: strtol(arg1, &a1, 0);
 
     if (a0 == arg0 || *a0 != '\0' || n <= 0 || a1 == arg1) printf("Error: Unknown usage: '%s'\n", arg);
   
     vaddr_t start = cpu.pc;
     for (vaddr_t i = start; i < 0X88000000 ; i += 4) {
-      printf("1\n");
-      if (!(s != i)) {
-        if (s == 0) i = cpu.pc;
-        printf("1\n");
+      if (s == i) {
 	for (int c = 0; c < n; c ++) {
 	  vaddr_t current = i + c * 4;
 	  if (current >= 0X88000000) break;
