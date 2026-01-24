74212bdec881968cd9c199cff6bf610cb47c0115
tracer-ysyx <tracer@ysyx.org>
2026-01-24 11:09:49 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  11:09:49 up 6 days, 17:03,  1 user,  load average: 2.21, 2.09, 2.04

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -115,9 +115,9 @@ static bool make_token(char *e) {
         char *substr_start = e + position;
         int substr_len = pmatch.rm_eo;
 
-        Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
+        /*Log("match rules[%d] = \"%s\" at position %d with len %d: %.*s",
             i, rules[i].regex, position, substr_len, substr_len, substr_start);
-          
+        */  
         switch (rules[i].token_type) { //开始将匹配的type内容写入每个token中.
           default: if (rules[i].token_type != TK_NOTYPE) {
           	      tokens[n].type = rules[i].token_type;
@@ -126,7 +126,7 @@ static bool make_token(char *e) {
 		      char s[MAX];
 		      sprintf(s, "%.*s", substr_len, substr_start);
 		      strcpy(tokens[n].str, s);
-          	      //printf("tokens[%d].type: %d\ntokens[%d].str: %s\n", n, tokens[n].type, n, tokens[n].str);
+          	      printf("tokens[%d].type: %d\ntokens[%d].str: %s\n", n, tokens[n].type, n, tokens[n].str);
           	      n++;
         	    }
 
@@ -217,7 +217,7 @@ uint32_t eval(int p, int q) {
   }
   
   else {
-  op = m_op(p, q);
+    op = m_op(p, q);
     val1 = eval(p, op - 1);
     val2 = eval(op + 1, q);
 //    printf("Last..op: %s val1: %d val2: %d\n", tokens[op].str, val1, val2);
