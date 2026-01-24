0a23cc3127817c6b7f6d7a921b7d3014ebe07a90
tracer-ysyx <tracer@ysyx.org>
2026-01-24 10:28:08 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:28:08 up 6 days, 16:21,  1 user,  load average: 2.29, 2.13, 2.11

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -36,6 +36,10 @@ word_t isa_reg_str2val(const char *s, bool *success) {
       *success = true;
       return gpr(i);
     }
+    else if (strcmp(regs[i], "pc") == 0) {
+      *success = true;
+      return cpu.pc;
+    }
   }
   printf("no this reg!\n");
   return 0;
