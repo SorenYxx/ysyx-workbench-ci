4c6d9a9cf3e3ff926d93174fb0255867e5686375
tracer-ysyx <tracer@ysyx.org>
2026-01-24 10:44:00 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  10:44:00 up 6 days, 16:37,  1 user,  load average: 2.09, 2.07, 2.07

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -80,10 +80,9 @@ static void execute(uint64_t n) {
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
     g_nr_guest_inst ++;
-    printf("this\n");
     trace_and_difftest(&s, cpu.pc);
-    printf("this\n");
     if (nemu_state.state != NEMU_RUNNING) break;
+    printf("this\n");
     IFDEF(CONFIG_DEVICE, device_update());
   }
 }
