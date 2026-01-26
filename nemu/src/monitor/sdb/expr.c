4b000c13eb90a1ff088c988ee0d1500121b536ce
tracer-ysyx <tracer@ysyx.org>
2026-01-26 08:54:06 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  08:54:06 up 1 day, 16:43,  1 user,  load average: 0.59, 0.48, 0.63

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -127,6 +127,10 @@ static bool make_token(char *e) {
 		      strcpy(tokens[n].str, s);
           	      //printf("tokens[%d].type: %d\ntokens[%d].str: %s\n", n, tokens[n].type, n, tokens[n].str);
           	      n++;
+		      if (n >= MAX) {
+		        printf("Too many tokens!\n");
+  		        return false;
+			}
         	    }
 
         position += substr_len;
@@ -238,6 +242,8 @@ uint32_t eval(int p, int q) {
 }
 
 word_t expr(char *e, bool *success) {
+//  n = 0;
+
   if (!make_token(e)) {
     *success = false;
     return 0;
