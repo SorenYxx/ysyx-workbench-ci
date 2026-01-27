d3a37ce86f344efc10aa71dcf8b3c7fe7e5b2f1c
tracer-ysyx <tracer@ysyx.org>
2026-01-27 17:26:51 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:26:51 up 3 days,  1:16,  1 user,  load average: 0.63, 0.59, 0.76

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -231,7 +231,9 @@ uint32_t eval(int p, int q) {
       case TK_AND: return val1 && val2;
       case TK_EQ: return val1 == val2;
       case TK_NEQ: return val1 != val2;
-      case DEREF: uint32_t addr = eval(p + 1, q); return vaddr_read(addr, 4);
+      case DEREF: uint32_t addr = eval(p + 1, q);
+		  printf("0x%08x: 0x%08x", addr, vaddr_read(addr, 4));
+	          return vaddr_read(addr, 4);
       default: assert(0);
     }
   }
