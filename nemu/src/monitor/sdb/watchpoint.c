4e5bdc685b401f2b3e4038336034e104a81335af
tracer-ysyx <tracer@ysyx.org>
2026-01-27 14:16:42 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:16:42 up 2 days, 22:06,  1 user,  load average: 0.84, 0.89, 0.84

--- nemu/src/monitor/sdb/watchpoint.c
+++ nemu/src/monitor/sdb/watchpoint.c
@@ -147,7 +147,7 @@ int c_print() {
 }
 
 int c_w(char *args) {
-  assert(args != NULL); 
+  if (args == NULL) return 0;
   WP* p = new_wp(args);
 
   printf("Set a watchpoint[%d]: %s\n", p->NO, p->str);
