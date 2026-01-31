aa967599e89ad4ae9c1dabfc7f69b415dd5095f2
tracer-ysyx <tracer@ysyx.org>
2026-01-31 17:10:52 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:10:52 up 7 days,  9:00,  1 user,  load average: 0.34, 0.64, 0.67

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <am.h>
 #include <nemu.h>
 
@@ -9,6 +8,4 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
 
   kbd->keydown = (k & KEYDOWN_MASK ? true : false);
   kbd->keycode = k & ~KEYDOWN_MASK;
-
-  printf("is %02x status: %d\n", kbd->keycode, kbd->keydown);
 }
