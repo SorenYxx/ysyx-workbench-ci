cff2f015e3ff0945d2482bda5b0113fd977ff8e0
tracer-ysyx <tracer@ysyx.org>
2026-02-02 00:25:12 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:25:12 up 8 days, 16:15,  1 user,  load average: 0.39, 0.54, 0.70

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -24,11 +24,6 @@ static uint8_t *pmem = NULL;
 static uint8_t pmem[CONFIG_MSIZE] PG_ALIGN = {};
 #endif
 
-static int a = 0;
-#ifdef CONFIG_MTRACE
-a = 1;
-#endif
-
 uint8_t* guest_to_host(paddr_t paddr) { return pmem + paddr - CONFIG_MBASE; }
 paddr_t host_to_guest(uint8_t *haddr) { return haddr - pmem + CONFIG_MBASE; }
 
@@ -57,22 +52,36 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) {
-    if (a) printf("read          0x%08x from 0x%08x\n", pmem_read(addr, len), addr);
+#ifdef CONFIG_MTRACE
+    printf("read          0x%08x from 0x%08x\n", pmem_read(addr, len), addr);
+#endif
     return pmem_read(addr, len);
   }
 
-  IFDEF(CONFIG_DEVICE, if (a) printf("(device)read  0x%08x from 0x%08x\n", mmio_read(addr, len), addr); return mmio_read(addr, len));
+  IFDEF(CONFIG_DEVICE, 
+#ifdef CONFIG_MTRACE 
+  printf("(device)read  0x%08x from 0x%08x\n", mmio_read(addr, len), addr); 
+#endif
+  return mmio_read(addr, len));
+
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { 
-    if (a) printf("write         0x%08x to   0x%08x\n", data, addr);
+#ifdef CONFIG_MTRACE 
+    printf("write         0x%08x to   0x%08x\n", data, addr);
+#endif
     pmem_write(addr, len, data); 
     return;
   }
 
-  IFDEF(CONFIG_DEVICE, if (a) printf("(device)write 0x%08x to   0x%08x\n", data, addr); mmio_write(addr, len, data); return);
+  IFDEF(CONFIG_DEVICE, 
+#ifdef CONFIG_MTRACE
+  printf("(device)write 0x%08x to   0x%08x\n", data, addr); 
+#endif
+  mmio_write(addr, len, data); return);
+
   out_of_bound(addr);
 }
