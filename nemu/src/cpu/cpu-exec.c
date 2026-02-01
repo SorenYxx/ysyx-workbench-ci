b8f808687d1e98fe3472dad32d3405ff7f0910d6
tracer-ysyx <tracer@ysyx.org>
2026-02-01 16:16:26 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:16:26 up 8 days,  8:06,  1 user,  load average: 0.41, 0.44, 0.49

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -54,11 +54,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
   isa_exec_once(s);
 
   if (inde <= 10) buf[inde ++] = cpu.pc;
-    else {
-      for (int i = 0; i < 9; i ++) { buf[i] = buf[i + 1]; }
-      buf[10] = cpu.pc;
-    }
-  printf("%08x    buf[%d]: 0x%08x\n", cpu.pc, inde, buf[inde]);
+  else {
+    for (int i = 0; i < 9; i ++) { buf[i] = buf[i + 1]; }
+    buf[10] = cpu.pc;
+  }
+  printf("0x%08x    buf[%d]: 0x%08x\n", cpu.pc, inde, buf[inde-1]);
+
   cpu.pc = s->dnpc;
 #ifdef CONFIG_ITRACE
   char *p = s->logbuf;
@@ -90,13 +91,6 @@ static void execute(uint64_t n) {
   Decode s;
   for (;n > 0; n --) {
     exec_once(&s, cpu.pc);
-     
-    /*if (inde <= 10) buf[inde ++] = cpu.pc;
-    else { 
-      for (int i = 0; i < 9; i ++) { buf[i] = buf[i + 1]; }
-      buf[10] = cpu.pc;
-    }
-    printf("%08x    buf[%d]: 0x%08x\n", cpu.pc, inde, buf[inde]);*/
     g_nr_guest_inst ++;
     trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;
