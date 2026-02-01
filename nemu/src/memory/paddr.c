ba2cb294d50f3467cd0468fb825566191e159bae
tracer-ysyx <tracer@ysyx.org>
2026-02-01 17:05:14 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  17:05:14 up 8 days,  8:55,  1 user,  load average: 0.51, 0.72, 0.61

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -56,18 +56,18 @@ void init_mem() {
 
 word_t paddr_read(paddr_t addr, int len) {
   if (likely(in_pmem(addr))) {
-    if (a) printf("read 0x%08x from 0x%08x\n", pmem_read(addr, len), addr);
+    if (a) printf("read          0x%08x from 0x%08x\n", pmem_read(addr, len), addr);
     return pmem_read(addr, len);
   }
 
-  IFDEF(CONFIG_DEVICE, if (a) printf("(device)read 0x%08x from 0x%08x\n", mmio_read(addr, len), addr); return mmio_read(addr, len));
+  IFDEF(CONFIG_DEVICE, if (a) printf("(device)read  0x%08x from 0x%08x\n", mmio_read(addr, len), addr); return mmio_read(addr, len));
   out_of_bound(addr);
   return 0;
 }
 
 void paddr_write(paddr_t addr, int len, word_t data) {
   if (likely(in_pmem(addr))) { 
-    if (a) printf("write 0x%08x to 0x%08x\n", data, addr);
+    if (a) printf("write        0x%08x to 0x%08x\n", data, addr);
     pmem_write(addr, len, data); 
     return;
   }
