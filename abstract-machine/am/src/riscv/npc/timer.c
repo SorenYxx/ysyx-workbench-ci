4755a513212c7d60e6e42459eff66cb9b5027436
tracer-ysyx <tracer@ysyx.org>
2026-02-02 20:30:43 +0800

    > sim RTL ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  20:30:43 up 9 days, 12:20,  2 users,  load average: 0.41, 0.72, 0.94

--- abstract-machine/am/src/riscv/npc/timer.c
+++ abstract-machine/am/src/riscv/npc/timer.c
@@ -10,7 +10,7 @@ void __am_timer_uptime(AM_TIMER_UPTIME_T *uptime) {
   uint32_t low = inl(RTC_ADDR);
   uint32_t high = inl(RTC_ADDR + 4);
 
-  uptime->us = (((uint64_t)high << 32) | low) / 10;
+  uptime->us = ((uint64_t)high << 32) | low;
 }
 
 void __am_timer_rtc(AM_TIMER_RTC_T *rtc) {
