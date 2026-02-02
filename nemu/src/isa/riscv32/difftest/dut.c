c0802f7507e6deb19c4c662b52d98550217a69f1
tracer-ysyx <tracer@ysyx.org>
2026-02-02 14:46:29 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  14:46:29 up 9 days,  6:36,  2 users,  load average: 0.56, 0.64, 0.71

--- nemu/src/isa/riscv32/difftest/dut.c
+++ nemu/src/isa/riscv32/difftest/dut.c
@@ -26,7 +26,7 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   }
 
   if (ref_r->pc != cpu.pc) {
-    Log("PC mismatch! [REF] 0x%08x | [DUT] 0x%08x", ref_r->pc, pc);
+    Log("PC mismatch! [REF] 0x%08x | [DUT] 0x%08x", ref_r->pc, cpu.pc);
     return false;
   }
   return true;
