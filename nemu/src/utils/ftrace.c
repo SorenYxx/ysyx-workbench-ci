5479bcc87036f319bb64c7efaedfcbceb97eb81e
tracer-ysyx <tracer@ysyx.org>
2026-02-14 23:25:25 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  23:25:25 up  5:18,  1 user,  load average: 1.39, 1.24, 0.93

--- nemu/src/utils/ftrace.c
+++ nemu/src/utils/ftrace.c
@@ -70,9 +70,9 @@ int depth = 0;
 void ftrace_print(uint32_t pc, uint32_t target, bool is_call) {
   // printf("The target is 0x%08x\n", target);
   const char *funt_name = get_func(target);
+  printf("name: %s\n", funt_name);
   if (funt_name == NULL) return;
 #ifdef CONFIG_FTRACE
-  printf("GOOD\n");
   if (is_call) {
     printf("0x%08x: %*s call [%s@0x%08x]\n", pc, depth * 2, "", funt_name, target);
     depth ++;
