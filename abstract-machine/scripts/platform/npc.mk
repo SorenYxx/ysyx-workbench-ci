805d2708e4fbd057c818bd25e506df98fae953f8
tracer-ysyx <tracer@ysyx.org>
2026-02-14 21:43:33 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:43:33 up  3:36,  1 user,  load average: 0.73, 1.00, 1.01

--- abstract-machine/scripts/platform/npc.mk
+++ abstract-machine/scripts/platform/npc.mk
@@ -12,7 +12,6 @@ CFLAGS    += -fdata-sections -ffunction-sections
 LDSCRIPTS += $(AM_HOME)/scripts/linker.ld
 LDFLAGS   += --defsym=_pmem_start=0x80000000 --defsym=_entry_offset=0x0
 LDFLAGS   += --gc-sections -e _start
-# NPCFLAGS  += -d
 NPCFLAGS  += -b
 
 DUMP = $(shell find ./npc -name "wave.fst")
