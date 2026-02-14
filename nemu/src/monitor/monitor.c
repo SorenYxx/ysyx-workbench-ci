7cef30924bca73a4bae6c79f01e483b878b577bb
tracer-ysyx <tracer@ysyx.org>
2026-02-14 22:23:43 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  22:23:43 up  4:16,  1 user,  load average: 1.46, 1.17, 0.77

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -123,13 +123,13 @@ void init_monitor(int argc, char *argv[]) {
   /* Perform ISA dependent initialization. */
   init_isa();
 
+  /* Load the image to memory. This will overwrite the built-in image. */
+  long img_size = load_img();
+
   /* Initialize ftrace */
 #ifdef CONFIG_FTRACE
   init_ftrace(elf_file);
 #endif
-
-  /* Load the image to memory. This will overwrite the built-in image. */
-  long img_size = load_img();
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
