0c28921b1b7430f40324d6f106e22a0e1152900a
tracer-ysyx <tracer@ysyx.org>
2026-02-23 21:22:28 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.14.0-37-generic #37~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Nov 20 10:25:38 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  21:22:28 up  8:20,  1 user,  load average: 0.89, 0.64, 0.96

--- nemu/src/isa/riscv32/system/intr.c
+++ nemu/src/isa/riscv32/system/intr.c
@@ -18,6 +18,7 @@
 word_t isa_raise_intr(word_t NO, vaddr_t epc) {
   cpu.mepc = epc;
   cpu.mcause = NO;
+  printf("intr NO: %d, epc: 0x%08x, mtvec: 0x%08x\n", NO, epc, cpu.mtvec);
 
   return cpu.mtvec & ~0x3;
 }
