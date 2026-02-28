1ec7c8721335958641b97c46aa47c927c6d663f9
tracer-ysyx <tracer@ysyx.org>
2026-03-01 00:04:45 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 6.17.0-14-generic #14~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Thu Jan 15 15:52:10 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  00:04:45 up 1 day, 27 min,  1 user,  load average: 0.51, 0.48, 0.72

--- nemu/src/cpu/difftest/ref.c
+++ nemu/src/cpu/difftest/ref.c
@@ -31,7 +31,6 @@ __EXPORT void difftest_memcpy(paddr_t addr, void *buf, size_t n, bool direction)
 __EXPORT void difftest_regcpy(void *dut, bool direction) {
   if (direction == DIFFTEST_TO_REF) {
     memcpy(&cpu, dut, DIFFTEST_REG_SIZE);
-    printf("sizeof(DIFFTEST_REG_SIZE) = %ld\n", DIFFTEST_REG_SIZE);
   } else {
     memcpy(dut, &cpu, DIFFTEST_REG_SIZE);
   }
