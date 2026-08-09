bd71f9a59dc056694122ab6e941fe9d95bedf048
tracer-ysyx <tracer@ysyx.org>
2026-08-09 16:37:48 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:37:48 up  7:27,  1 user,  load average: 0.85, 0.78, 0.90

--- nemu/src/memory/paddr.c
+++ nemu/src/memory/paddr.c
@@ -107,7 +107,6 @@ word_t paddr_read(paddr_t addr, int len) {
 
   IFDEF(CONFIG_DEVICE, return mmio_read(addr, len));
   // Without device layer: return 0 for MMIO region
-  if (addr >= 0x02000000 && addr < 0x10020000) return 0;
   out_of_bound(addr);
   return 0;
 }
@@ -119,6 +118,5 @@ void paddr_write(paddr_t addr, int len, word_t data) {
 
   IFDEF(CONFIG_DEVICE, mmio_write(addr, len, data); return);
   // Without device layer: silently ignore MMIO writes
-  if (addr >= 0x02000000 && addr < 0x10020000) return;
   out_of_bound(addr);
 }
