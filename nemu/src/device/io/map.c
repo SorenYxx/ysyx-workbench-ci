cea324a0936be1d8efa79b97427f0b8bcadd433f
tracer-ysyx <tracer@ysyx.org>
2026-08-09 15:18:49 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:18:49 up  6:08,  1 user,  load average: 2.18, 1.90, 1.43

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -47,6 +47,7 @@ static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_wr
 }
 
 void init_map() {
+  printf("1\n");
   io_space = malloc(IO_SPACE_MAX);
   assert(io_space);
   p_space = io_space;
