f909e397909f0e9191e76952f63b5267d5ff4727
tracer-ysyx <tracer@ysyx.org>
2026-08-09 15:13:21 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:13:21 up  6:02,  1 user,  load average: 2.69, 1.84, 1.24

--- nemu/src/monitor/monitor.c
+++ nemu/src/monitor/monitor.c
@@ -49,11 +49,12 @@ static char *elf_file = NULL;
 static int difftest_port = 1234;
 
 static long load_img() {
+  printf("1\n");
   if (img_file == NULL) {
     Log("No image is given. Use the default build-in image.");
     return 4096; // built-in image size
   }
-  
+
   printf("1\n");
   FILE *fp = fopen(img_file, "rb");
   Assert(fp, "Can not open '%s'", img_file);
