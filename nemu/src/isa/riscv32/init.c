625ef94f557ca64fe1544c325c19b759753288a7
tracer-ysyx <tracer@ysyx.org>
2026-08-09 15:20:52 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  15:20:52 up  6:10,  1 user,  load average: 2.36, 2.01, 1.52

--- nemu/src/isa/riscv32/init.c
+++ nemu/src/isa/riscv32/init.c
@@ -36,9 +36,7 @@ static void restart() {
 
 void init_isa() {
   /* Load built-in image. */
-  printf("1\n");
-  memcpy(guest_to_host(RESET_VECTOR), img, sizeof(img));
-  printf("1\n");
+  memcpy(guest_to_host_flash(RESET_VECTOR), img, sizeof(img));
 
   /* Initialize this virtual computer system. */
   restart();
