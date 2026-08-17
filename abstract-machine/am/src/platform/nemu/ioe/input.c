1ad8dd1906f101aaa299362ace24879e7e6250e0
tracer-ysyx <tracer@ysyx.org>
2026-08-17 16:04:53 +0800

    > compile NEMU ysyx_22040000 甘远祥 Linux sorenYxx 7.0.0-28-generic #28~24.04.1-Ubuntu SMP PREEMPT_DYNAMIC Wed Jul  1 15:50:57 UTC 2 x86_64 x86_64 x86_64 GNU/Linux  16:04:53 up  6:42,  1 user,  load average: 0.74, 1.04, 1.15

--- abstract-machine/am/src/platform/nemu/ioe/input.c
+++ abstract-machine/am/src/platform/nemu/ioe/input.c
@@ -9,3 +9,8 @@ void __am_input_keybrd(AM_INPUT_KEYBRD_T *kbd) {
   kbd->keydown = (k & KEYDOWN_MASK ? true : false);
   kbd->keycode = k & ~KEYDOWN_MASK;
 }
+
+void __am_uart_input(AM_UART_RX_T *uart) {
+  if (inb(MMIO_BASE) & 0x01) uart->data = inb(MMIO_BASE);
+  else uart->data = 0xff;
+}
